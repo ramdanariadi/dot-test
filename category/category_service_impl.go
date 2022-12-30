bab4591f89667804ed907309ea3f751d98d69775
Ramdan Ariadi <ramadanariadi@gmail.com>
2022-12-30 14:11:58 +0800

    cache category

--- category/category_service_impl.go
+++ category/category_service_impl.go
@@ -1,27 +1,48 @@
 package category
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
 	"github.com/ramdanariadi/dot-test/helpers"
 	"gorm.io/gorm"
 	"log"
+	"time"
 )
 
 type CategoryServiceImpl struct {
-	DB *gorm.DB
+	DB          *gorm.DB
+	RedisClient *redis.Client
 }
 
 func NewCategoryService(DB *gorm.DB) *CategoryServiceImpl {
 	return &CategoryServiceImpl{
-		DB: DB,
+		DB:          DB,
+		RedisClient: helpers.NewRedisClient(),
 	}
 }
 
 func (c *CategoryServiceImpl) FindById(id string) *Category {
 	categoryModel := Category{}
-	first := c.DB.First(&categoryModel, "id = ?", id)
-	if first.Error != nil {
-		return nil
+	ctx := context.Background()
+	cache, err := c.RedisClient.Get(ctx, id).Result()
+	helpers.LogIfError(err)
+
+	if cache != "" {
+		log.Print("category found in cache")
+		err := json.Unmarshal([]byte(cache), &categoryModel)
+		helpers.LogIfError(err)
+	} else {
+		first := c.DB.First(&categoryModel, "id = ?", id)
+		if first.Error != nil {
+			return nil
+		}
+		bytes, err := json.Marshal(categoryModel)
+		helpers.LogIfError(err)
+		err = c.RedisClient.Set(ctx, categoryModel.ID, bytes, 1*time.Hour).Err()
+		helpers.LogIfError(err)
+		log.Print("category in cache")
 	}
 	return &categoryModel
 }
@@ -50,8 +71,10 @@ func (c *CategoryServiceImpl) Update(request *CategoryDTO, id string) {
 	}
 	category.Category = request.Category
 	c.DB.Save(&category)
+	c.RedisClient.Del(context.Background(), id)
 }
 
 func (c *CategoryServiceImpl) Delete(id string) {
 	c.DB.Where("id = ?", id).Delete(&Category{})
+	c.RedisClient.Del(context.Background(), id)
 }
